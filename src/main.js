b6d72e31c43bab1956345b6a46bd1c106b49dd04
Lerryteng <wangyiteng2927@163.com>
2019-11-25 18:23:42 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import 'lib-flexible';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/.invoke/router.js';
+import './router/router.interceptor';
+import './components/global';
+import './common/errorPlugin';
+import './icons';
+import './filters';
+import './services';
+
+/* eslint-disable */
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  // use Runtime-only
+  // https://vuejs.org/v2/guide/installation.html
+  render: (h) => h(App)
+});
